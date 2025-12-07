d8dd512269e529beb185f09ea801d72b0e2c6601
Mayank Singh <mayanksingh@Mayanks-MacBook-Pro.local>
2025-12-07 18:47:50 +0530

    Updated web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3654498723"
+    serviceWorkerVersion: "3963814807"
   }
 });
