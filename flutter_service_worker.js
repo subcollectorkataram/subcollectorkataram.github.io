dbfd00117fdc57e7f7fb2b90043f625bba41b6c7
Mayank Singh <mayanksingh@Mayanks-MacBook-Pro.local>
2025-12-12 13:50:09 +0530

    Flutter Base 1st Upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "7959e95f380386ab9789c56a79a6f806",
+const RESOURCES = {"flutter_bootstrap.js": "bc3605ee69a2aee38e0dc870760eeede",
 "version.json": "683848cd93fab2ca43475b2b27c4c16a",
 "index.html": "df44d677d83a1896323fe712f13e38cd",
 "/": "df44d677d83a1896323fe712f13e38cd",
-"main.dart.js": "1fa5cd137ffcc8f0dbf09c50e5ace265",
+"main.dart.js": "64088c41b6d33d43d417d987fcc74526",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
